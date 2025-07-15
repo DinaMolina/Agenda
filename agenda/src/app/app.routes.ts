37267e8bf81d8b4cb166fa5d0bff146925a3ab21
DinaMolina <48228193+DinaMolina@users.noreply.github.com>
2025-07-14 21:41:00 -0600

    Creacion del formulario de contacto

--- agenda/src/app/app.routes.ts
+++ agenda/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ContactoForm } from './features/contacto-form/contacto-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+    path: '',
+    component: ContactoForm
+  },
+  {
+    path: 'nuevo-contacto',
+    component: ContactoForm
+  }
+];
---
+++ agenda/src/app/features/contacto-form/contacto-form.ts
@@ -0,0 +1,32 @@
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-contacto-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './contacto-form.html',
+  styleUrl: './contacto-form.css'
+})
+export class ContactoForm {
+
+ private fb = inject(FormBuilder)
+
+ formContacto: FormGroup = this.fb.group({
+  nombre: ['', [Validators.required, Validators.minLength(3)]],
+  email: ['', [Validators.required]],
+  telefono: [''],
+  mensaje: ['', [Validators.required, Validators.minLength(50)]],
+  terminos: [false, Validators.required]
+ })
+
+ isValidField(fieldName: string): boolean | null {
+  return !!this.formContacto.controls[fieldName].errors
+ }
+
+ saveContacto(){
+  if (this.formContacto.invalid) {
+    this.formContacto.markAllAsTouched()
+    return;
+  }
+ }
+}
